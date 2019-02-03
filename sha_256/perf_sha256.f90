b81eb3843f0c40294d23c008df90cc0a4ad7a6a0
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-02-03 21:50:22 +0100

    Also does a performance test on a minimal hash.

--- sha_256/perf_sha256.f90
+++ sha_256/perf_sha256.f90
@@ -30,4 +30,22 @@ program main
   end if
   write(*,"(a,i0,a,i0,a,f10.6,a)") "Digested ",n," messages of ",len_trim(theMessage)," bytes in ",tEnd-tStart," seconds."
 
+  theMessage  = "42"
+  hashCorrect = "73475CB40A568E8DA8A045CED110137E159F890AC4DA883B6B17DC651B3A8049"
+
+  call cpu_time(tStart)
+  do i=1,n
+    call sha256_init(trim(theMessage))
+    call sha256_hash
+    call sha256_digest(hashDigest)
+  end do
+  call cpu_time(tEnd)
+
+  if(hashDigest == hashCorrect) then
+    write(*,"(a)") "SHA-256 Match !"
+  else
+    write(*,"(a)") "SHA-256 Failed :("
+  end if
+  write(*,"(a,i0,a,i0,a,f10.6,a)") "Digested ",n," messages of ",len_trim(theMessage)," bytes in ",tEnd-tStart," seconds."
+
 end program main
