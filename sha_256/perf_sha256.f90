36fe58460dcf6b130e815fe38c417b605d9d870b
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-02-03 21:54:40 +0100

    Added three performance tests of varying length.

--- sha_256/perf_sha256.f90
+++ sha_256/perf_sha256.f90
@@ -4,48 +4,37 @@ program main
 
   implicit none
 
-  integer, parameter  :: n = 1000000
-  character(len=1024) :: theMessage
-  character(len=64)   :: hashDigest, hashCorrect
-  integer :: i
+  integer, parameter  :: n = 3, num = 1000000
+  character(len=1024) :: wBuffer, theMessage(n)
+  character(len=64)   :: hashDigest, hashCorrect(n)
+  integer :: i, j
   real    :: tStart, tEnd
 
   open(11,file="lipsum.txt")
-  read(11,"(a1024)") theMessage
+  read(11,"(a1024)") wBuffer
   close(11)
-  hashCorrect = "B6ACA4145A1DB0A151E1569F3B197E17ABD6329807BFFC531AB22053554066A7"
+  theMessage(1)  =  wBuffer
+  hashCorrect(1) = "B6ACA4145A1DB0A151E1569F3B197E17ABD6329807BFFC531AB22053554066A7"
 
-  call cpu_time(tStart)
+  theMessage(2)  = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
+  hashCorrect(2) = "248D6A61D20638B8E5C026930C3E6039A33CE45964FF2167F6ECEDD419DB06C1"
+
+  theMessage(3)  = "abc"
+  hashCorrect(3) = "BA7816BF8F01CFEA414140DE5DAE2223B00361A396177A9CB410FF61F20015AD"
+
   do i=1,n
-    call sha256_init(trim(theMessage))
-    call sha256_hash
-    call sha256_digest(hashDigest)
+    call cpu_time(tStart)
+    do j=1,num
+      call sha256_init(trim(theMessage(i)))
+      call sha256_hash
+      call sha256_digest(hashDigest)
+    end do
+    call cpu_time(tEnd)
+
+    if(hashDigest /= hashCorrect(i)) then
+      write(*,"(a)") "SHA-256 Failed :("
+    end if
+    write(*,"(a,i0,a,i3,a,f10.6,a)") "Digested ",num," messages of ",len_trim(theMessage(i))," bytes in ",tEnd-tStart," seconds."
   end do
-  call cpu_time(tEnd)
-
-  if(hashDigest == hashCorrect) then
-    write(*,"(a)") "SHA-256 Match !"
-  else
-    write(*,"(a)") "SHA-256 Failed :("
-  end if
-  write(*,"(a,i0,a,i0,a,f10.6,a)") "Digested ",n," messages of ",len_trim(theMessage)," bytes in ",tEnd-tStart," seconds."
-
-  theMessage  = "42"
-  hashCorrect = "73475CB40A568E8DA8A045CED110137E159F890AC4DA883B6B17DC651B3A8049"
-
-  call cpu_time(tStart)
-  do i=1,n
-    call sha256_init(trim(theMessage))
-    call sha256_hash
-    call sha256_digest(hashDigest)
-  end do
-  call cpu_time(tEnd)
-
-  if(hashDigest == hashCorrect) then
-    write(*,"(a)") "SHA-256 Match !"
-  else
-    write(*,"(a)") "SHA-256 Failed :("
-  end if
-  write(*,"(a,i0,a,i0,a,f10.6,a)") "Digested ",n," messages of ",len_trim(theMessage)," bytes in ",tEnd-tStart," seconds."
 
 end program main
